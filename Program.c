3ca53578e579c6b79d008eb4760eab8b043afd1a
Samreenbanu Mahaboobali Havaldar <samreenbanu.mahaboobali.havaldar@pwc.com>
2026-04-20 19:24:57 +0530

    update

--- Program.c
+++ Program.c
@@ -4,10 +4,19 @@ int main() {
     int value1, value2, sum;
 
     printf("Enter first value: ");
-    scanf("%d", &value1);
+    if (scanf("%d", &value1) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
 
     printf("Enter second value: ");
-    scanf("%d", &value2);
+    if (scanf("%d", &value2) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    sum = value1 + value2;
+    printf("Sum: %d\n", sum);
 
     return 0;
 }
